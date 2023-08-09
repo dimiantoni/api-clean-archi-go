a8f98c1ecca4d71e1d464206c6083445c289e1ad
Dimi Vargas <dimi.vargas@boxdelivery.com.br>
2023-08-09 03:12:15 -0300

    remove ops folder

--- ops/db/init.sql
+++
@@ -1,3 +0,0 @@
-create database if not exists local_dev_database;
-use local_dev_database;
-create table if not exists user (id varchar(50), email varchar(255), password varchar(255), name varchar(100), address varchar(100), age INT(3), created_at datetime, updated_at datetime, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
